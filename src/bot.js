ffa05137f703e29e884af8d34a6631c94fe4f0bf
PalaceRogue <118322378+AdamUMS@users.noreply.github.com>
2023-04-29 11:02:11 +0800

    Updated bot.js code
    Used a code to display price in status

--- src/bot.js
+++ src/bot.js
@@ -1,20 +1,22 @@
+const Discord = require('discord.js');
 const fetch = require('node-fetch');
-const API_KEY = '815f72e5-7f31-40a8-8646-45974f666644';
+
+const client = new Discord.Client();
+const API_KEY = '815f72e5-7f31-40a8-8646-45974f666644'; //CMC API KEY
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 async function handlePriceCommand(message, args) {
+  async function handlePriceCommand(message, args) {
     const symbol = 'DST'; // Set the cryptocurrency symbol to DST
     const response = await fetch(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${symbol}&convert=USD`, {
       headers: {
         'X-CMC_PRO_API_KEY': API_KEY
-      }
+      }  
     });
-    const data = await response.json();
-    const price = data.data[symbol].quote.USD.price.toFixed(2);
-    message.channel.send(`The current price of ${symbol} is $${price}`);
-    client.user.setActivity(`DST: $${price}`, { type: 'WATCHING' }); // Update bot status with DST price
   }
+}
 
-  const commands = {
-    'ping': handlePingCommand,
-    'price': handlePriceCommand
-  };
+client.login('YOUR_DISCORD_BOT_TOKEN_HERE');
